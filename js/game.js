a9e7c8a0b4b0ca413c23496f7e0ff56da4050c5f
Chris <chris@cabtdesigns.com>
2021-08-19 00:02:20 -0700

    added event handler to the new game button

--- js/game.js
+++ js/game.js
@@ -1,6 +1,12 @@
 var BubbleShoot = window.BubbleShoot || {};
 BubbleShoot.Game = (function($){
-    var Game = function(){};
-    return Game;
+    var Game = function(){
+        this.init = function(){
 
+            $(".but_game_start").bind("click",startGame);
+        };
+        var startGame = function(){
+        };
+    };
+    return Game;
 }) (jQuery);
